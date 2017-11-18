9e0ae2fd955182a6e081b12a179d82866e250239
Anett_Soos <anett.soos@city.ac.uk>
2017-11-18 10:42:07 +0000

    initial array

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
-console.log("boo");
+let i,
+  j,
+  n = 30,
+  ewArray = [],
+  arrayRow = [],
+  arraColumn = [];
+
+let prevArray = new Array();
+for (i= 0; i < n; i++){
+  prevArray[i] = new Array();
+    for (j= 0; j < n; j++){
+      prevArray[i][j] = Math.floor(Math.random()*2);
+    }
+}
+
+let newArray = prevArray.map(prevArray[i][j] = Math.floor(Math.random()*2))
+
+console.log(prevArray);
+console.log(newArray);
