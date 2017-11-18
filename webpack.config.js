5edf2d5c729d91df30e507e6b15cfc06c1687fc1
Anett_Soos <anett.soos@city.ac.uk>
2017-11-18 10:06:30 +0000

    files setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        path: __dirname + '/build/assets',
+        filename: 'prod-main.js',
+        publicPath: 'index.html'
+    },
+    devServer: {
+        inline: true,
+        port: 3000
+    },
+    module: {
+         loaders: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react']
+            }
+          }
+        ]
+    }
+ };
