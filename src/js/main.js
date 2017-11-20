44f03c3b84fb8dae2737c81344148c01fb8b6a5c
Anett_Soos <anett.soos@city.ac.uk>
2017-11-20 09:03:56 +0000

    2 lines of code

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log("boo");
+let prevArray = (size) => new Array(size).fill(0).map((x) => x = Math.floor(Math.random()*2) );
+let grid = (size) => new Array(size).fill(size).map(prevArray);
+
+/* 
+if cell is 1 {
+2 =< live neighbours => 0
+=< 1  live neigbour => 1
+}
+if cell is 0 {
+  2 =< live neighbours => 1
+}
+*/
